c55f9b8d16208f867e106e2397184bcb7ec50fc7
gomesfilipe <filipejvmf@gmail.com>
2023-11-14 12:14:40 -0300

    wip: swarm class

--- t2-titmetable/src/Particle.py
+++ t2-titmetable/src/Particle.py
@@ -15,14 +15,18 @@ class Particle(TimeTable):
   a3: int = 2
   a4: int = 1
 
-  GBest: Union[List[Slot], None] = None
-  GBestValue: Union[float, None] = None
-
   def __init__(self, instance: Instance) -> None:
     super().__init__(instance)
     super().fill()
+    self.__value = float('inf')
     self.__PBest = super().get_copy_slots()
-    self.__PBestValue: Union[float, None] = self.fitness()
+    self.__PBestValue: Union[float, None] = self.__value
+
+  def get_value(self) -> float:
+    return self.__value
+
+  def get_pbest_value(self) -> float:
+    return self.__PBest
 
   def __str__(self) -> str:
     return super().__str__() + f'\nfitness: {self.fitness()}'
@@ -36,7 +40,9 @@ class Particle(TimeTable):
     x3 = Particle.a3 * self.compute_s3()
     x4 = Particle.a4 * self.compute_s4()
 
-    return x1 + x2 + x3 + x4
+    value = x1 + x2 + x3 + x4
+    self.__value = value
+    return value
 
   def rand_mutate(self) -> None:
     pass
@@ -52,18 +58,17 @@ class Particle(TimeTable):
     self.rand_change_pbest()
     self.rand_change_gbest()
 
-  def update_pbest(self, value) -> None:
-    if value > self.__PBestValue:
-      self.__PBestValue = value
+  def update_pbest(self) -> None:
+    if self.__value < self.__PBestValue:
+      self.__PBestValue = self.__value
       self.__PBest = super().get_copy_slots()
 
-
-path = '../instances/toy.ctt'
-instance = Instance(path)
+# path = '../instances/toy.ctt'
+# instance = Instance(path)
 # print(instance)
-tt = Particle(instance)
+# tt = Particle(instance)
 # tt.fill()
-print(tt)
+# print(tt)
 
 # print(tt.compute_s1())
 # print(tt.compute_s2())
--- t2-titmetable/src/Swarm.py
+++ t2-titmetable/src/Swarm.py
@@ -1,7 +1,12 @@
 from Particle import Particle
 from Instance import Instance
+from Slot import Slot
+from typing import Union, List
 
 class Swarm:
+  GBest: Union[List[Slot], None] = None
+  GBestValue: float('inf')
+
   def __init__(self, population: int, instance: Instance, w: int = 1, c1: int = 1, c2: int = 1, a1: int = 1, a2: int = 5, a3: int = 2, a4: int = 1) -> None:
     Particle.w = w
     Particle.c1 = c1
@@ -16,3 +21,8 @@ class Swarm:
 
   def __str__(self) -> str:
     return '\n'.join([p.__str__() + '\n' for p in self.__particles])
+
+  def update_gbest(self, particle: Particle) -> None:
+    if particle.get_pbest_value() < Swarm.GBestValue:
+      Swarm.GBest = particle.get_copy_slots()
+      Swarm.GBestValue = particle.get_pbest_value()
